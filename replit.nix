eec0ae493172c234de0d8d464a7a4e898d9613e0
Remy Skelton <124379820+Messi-Sui@users.noreply.github.com>
2023-02-15 16:16:48 +0000

    Finished the first part of unit-03

--- replit.nix
+++ replit.nix
@@ -1,5 +1,11 @@
 { pkgs }: {
   deps = [
+    pkgs.nodejs
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs
+    pkgs.nodejs
+    pkgs.nodejs,out
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
